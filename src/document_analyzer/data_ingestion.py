3072105d9e8c33ebb1b0252fdcda5ff859bfac7c
shivpalSW <shivpal.wadkar2050@gmail.com>
2025-08-13 15:16:20 +0530

    `tested session-based organization and logging to DocumentHandler class`

--- src/document_analyzer/data_ingestion.py
+++ src/document_analyzer/data_ingestion.py
@@ -6,26 +6,57 @@ from logger .custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 
 class DocumentHandler():
-
      """
-     A class to handle document ingestion and processing using PyMuPDF.
+     Handles PDF saving and reading operations.
+     Automatically logs all actions and supports session-based organization.
      """
+     def __init__(self,data_dir=None,session_id=None):
+        try:
+            self.log=CustomLogger().get_logger(__name__)
+            self.data_dir = data_dir or os.getenv(
+                "DATA_STORAGE_PATH",
+                os.path.join(os.getcwd(), "data", "document_analysis")
+            )
+            self.session_id = session_id or f"session_{datetime.utcnow().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
+            
+            # Create base session directory
+            self.session_path = os.path.join(self.data_dir, self.session_id)
+            
+            os.makedirs(self.session_path, exist_ok=True)
 
-     def _init__(self, document_path: str):
-         """
-         Initializes the DocumentHandler with a document path.
-         :param document_path: Path to the document to be processed.
-         """
-         pass
+            self.log.info("PDFHandler initialized", session_id=self.session_id, session_path=self.session_path)
 
-     def save_pdf(self):
-          """
-          Saves the PDF document to a specified location with a unique filename."""
-          pass
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentHandler: {e}")
+            raise DocumentPortalException("Error initializing DocumentHandler", e) from e
+        
+
+     def save_pdf(self,pdf_data):
+        """
+        Saves the PDF data to a file in the session directory.        
+        Saves the PDF document to a specified location with a unique filename.
+        pass
+        """
+        try:
+            pass
+        
+        except Exception as e:
+            self.log.error(f"Error saving PDF: {e}")
+            raise DocumentPortalException("Error saving PDF", e) from e
+
+     
 
      def read_pdf(self):
          """
          Reads the PDF document and extracts text from each page.
          :return: A list of strings, each representing the text of a page.
          """
-         pass
+         try:
+             pass
+         
+         except Exception as e:
+             self.log.error(f"Error saving PDF: {e}")
+             raise DocumentPortalException("Error saving PDF", e) from e
+         
+if __name__ == "__main__":
+    handler = DocumentHandler()

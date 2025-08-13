63e152b2ca1800cbeeab7fcb79c8135bed724da0
shivpalSW <shivpal.wadkar2050@gmail.com>
2025-08-13 17:04:26 +0530

    Added Pydantic Metadata model and enhance DocumentAnalyzer initialization with logging and error handling

--- model/models.py
+++ model/models.py
@@ -1,2 +1,14 @@
 from pydantic import BaseModel, RootModel,Field
 from typing import Optional,List,Dict, Union,Any
+
+
+class Metadata(BaseModel):
+    Summary: List[str] = Field(default_factory=list, description="Summary of the document")
+    Title: str
+    Author: str
+    DateCreated: str   
+    LastModifiedDate: str
+    Publisher: str
+    Language: str
+    PageCount: Union[int, str]  # Can be "Not Available"
+    SentimentTone: str 
--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,26 +1,46 @@
 import os
+import sys
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
+from prompt.prompt_library import *
 
 
-class DocumentAnalyzer():
+class DocumentAnalyzer:
+    """
+    # A class to handle document analysis and metadata extraction.
+    # This class uses LangChain to analyze documents and extract metadata.
+    """
 
-     """
-     # A class to handle document analysis and metadata extraction.
-     # This class uses LangChain to analyze documents and extract metadata.
-     """
+    def __init__(self):
+        """
+        Initialize the DocumentAnalyzer class.
+        This method sets up the necessary components for document analysis.
+        """
+        self.log = CustomLogger().get_logger(__name__)
+        try:
+            self.loader = ModelLoader()
+            self.llm = self.loader.load_llm()
 
-     def __init__(self):
-          pass
+            # Prepare parsers
+            self.parser = JsonOutputParser(pydantic_object=Metadata)
+            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
 
-     def analyze_document(self):
-         """
-         Analyzes the metadata of the document.
-         :param document_path: Path to the document to be analyzed.
-         :return: Metadata of the document.
-         """
-         pass
+            self.prompt = prompt
+
+            self.log.info("DocumentAnalyzer initialized successfully")
+
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentAnalyzer: {e}")
+            raise DocumentPortalException("Error in DocumentAnalyzer initialization", sys)
+
+    def analyze_document(self):
+        """
+        Analyzes the metadata of the document.
+        :param document_path: Path to the document to be analyzed.
+        :return: Metadata of the document.
+        """
+        pass

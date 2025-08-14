14e7d0eb5b016c4a2be333fad9d194cf201cdfcf
shivpalSW <shivpal.wadkar2050@gmail.com>
2025-08-14 17:32:24 +0530

    Refactored DocumentComparator class to DocumentIngestion class with added reading pdf functionality and error handling.

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -4,30 +4,55 @@ import fitz
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 
-class DocumentComparator:
-    
-     """
-     # A class to handle document comparison and metadata extraction.
-     # This class uses LangChain to compare documents and extract differences.
-     # It also uses PyMuPDF to extract metadata from PDF documents.
-     """
-    def __init__(self):
-        pass
-    
+class DocumentIngestion:
+    """
+    # A class to handle document comparison and metadata extraction.
+    # This class uses LangChain to compare documents and extract differences.
+    # It also uses PyMuPDF to extract metadata from PDF documents.
+    """
+    def __init__(self,base_dir):
+        # Initialize the DocumentIngestion class with a base directory.
+        self.log = CustomLogger().get_logger(__name__)
+        self.base_dir = Path(base_dir)
+        self.base_dir.mkdir(parents=True, exist_ok=True) 
+
     def delete_existing_files(self):
         """
         Deletes existing files at the specified paths.
         """
-        pass
+        
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error deleting existing files: {e}")
+            raise DocumentPortalException("An error occurred while deleting existing files.", sys)
      
     def save_uploaded_files(self):
         """
         Saves uploaded files to a specific directory.
         """
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error saving uploaded files: {e}")
+            raise DocumentPortalException("An error occurred while saving uploaded files.", sys)
     
-    def read_pdf(self):
+    def read_pdf(self,pdf_path: Path)->str:
         """
         Reads a PDF file and extracts text from each page.
         """
-        pass
+        try:
+             with fitz.open(pdf_path) as doc:
+                if doc.is_encrypted:
+                    raise ValueError(f"PDF is encrypted: {pdf_path.name}")
+                all_text = []
+                for page_num in range(doc.page_count):
+                    page=doc.load_page(page_num)
+                    text = page.get_text() #type: ignore
+                    if text.strip():
+                        all_text.append(f"\n --- Page {page_num + 1} --- \n{text}")
+                self.log.info("PDF read successfully", file=str(pdf_path), pages=len(all_text))
+                return "\n".join(all_text)
+        except Exception as e:
+            self.log.error(f"Error reading PDF: {e}")
+            raise DocumentPortalException("An error occurred while reading the PDF.", sys) 
